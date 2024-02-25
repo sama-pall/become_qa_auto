fa65326e89db95020c40574015436f7ba52d49f2
Dmytro Zhdanov <zhd.dmitry@gmail.com>
2024-02-25 18:17:04 +0200

    marks created, class User moved to conftest

---
+++ conftest.py
@@ -0,0 +1,24 @@
+import pytest
+
+
+class User:
+    def __init__(self):
+        self.name = None
+        self.second_name = None
+
+    def create(self):
+        self.name = 'Arthur'
+        self.second_name = 'Dent'
+
+    def remove(self):
+        self.name = ''
+        self.second_name = ''
+
+
+@pytest.fixture
+def user():
+    user = User()
+
+    user.create()
+    yield user
+    user.remove()
